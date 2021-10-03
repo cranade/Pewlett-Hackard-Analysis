6c2e509c5e18a8ee9bb6559c87b7682f33c3f228
cranade <chanderranade@gmail.com>
2021-10-02 23:05:02 -0400

    Message

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,4 +1,3 @@
--- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
@@ -25,7 +24,19 @@ CREATE TABLE salaries (
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
+);
+CREATE TABLE dept_emp (
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INT NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL 
+);
+CREATE TABLE titles(
+	emp_no INT NOT NULL,title VARCHAR,from_date DATE NOT NULL,
+	to_date DATE NOT NULL
 );
 SELECT * FROM departments;
